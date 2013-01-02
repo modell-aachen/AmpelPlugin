6166ef16fe4fbbf1d960d4e1a7c2fdd8fec9fbe3
Stephan Osthold <osthold@modell-aachen.de>
2013-01-02 10:24:27 +0100

    parseDate in Funktion ausgelagert und Code leicht umstrukturiert.

--- pub/System/AmpelPlugin/ampel_src.js
+++ pub/System/AmpelPlugin/ampel_src.js
@@ -11,6 +11,47 @@ function AmpelPluginRenderer($) {
         return imgTags[level] + altText + imgTags[4];
     }
 
+    function parseDate(str) {
+        var date = null;
+        try { 
+            // Datum rausssuchen
+            var split = /(\d\d?)\.(\d\d?)\.(\d{2,4})?/.exec(str);
+            if(split === null) {
+                // Englisches Format?
+                split = /(\d\d?) (\w{3}) (\d{2,4})/.exec(str);
+                if(split !== null) {
+                    var m = split[2];
+                    var monthArray = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "err");
+                    var gefunden = 0;
+                    for(var i = 0; i < monthArray.length; i++) {
+                        if(m == monthArray[i]) {
+                            m = i;
+                            gefunden = 1;
+                            break;
+                        }
+                    }
+                    if(gefunden == 0) return -1;
+                    date = new Date(split[3], m, split[1], 23, 59);
+                } else {
+                    return null;
+                }
+            } else {    
+                if(split[3] === undefined || split[3] === "") { // Falls ohne Jahr: 27.07.
+                    split[3] = datum.getFullYear(); // Kann auch 2-Stellig sein (IE)
+                }
+                if(split[3] < 2000) split[3] = Number(split[3]) + 2000; // JJ in JJJJ umwandeln
+
+                date = new Date(split[3], split[2]-1, split[1], 23, 59);
+            }
+        }
+        catch(e)
+        {
+            log("Error while parsing date '" + str + "': " + e);
+            return null;
+        }
+        return date;
+    }
+
     // Wenn dieser Test besteht, ist es hoffentlich ein Array
     if(typeof(AmpelData) !== "object" || AmpelData.length === undefined) return;
 
@@ -64,131 +105,101 @@ function AmpelPluginRenderer($) {
 
         // Tabelle raussuchen und bearbeiten
         var tabellen = document.getElementById(AmpelID);
-        if(tabellen !== null && tabellen !== undefined) {
-            // Wenn das Objekt mit AmpelID ein div ist, benutze (erste) Tabelle darin
-            if(tabellen.tagName.toUpperCase() == "DIV") {
-                tabellen = tabellen.getElementsByTagName("table");
-                if(tabellen.length != 0) {
-                    tabellen = tabellen[0];
-                } else {
-                    log("No table found in div with id '" + AmpelID + "'!");
-                    continue;
-                }
-            }
+        if(tabellen === null || tabellen === undefined) {
+            log("Id '" + AmpelID + "' not found!");
+            continue;
+        }
 
-            var zeilen = tabellen.rows; 
-
-            var head = zeilen[0]; // Kopfzeile
-
-            var termin = -1; // Spaltennummer Termin
-            var ampel = -1; // Spaltennummer Ampel
-            var done = -1; // Spaltennummer Erledigt
-            var reg = false; // RegEx zum pruefen von Erledigt
-
-            // Pruefe ob optionale Erledigt-Spalte vorhanden
-            if(AmpelDText != "" && AmpelDCond != "") {
-                reg = new RegExp(AmpelDCond,"i");
-            }    
-
-            // Suche Spaltennummern raus
-            for (var i = 0; i < head.cells.length; i++) {
-                var cell = head.cells[i].textContent || head.cells[i].innerText; // textContent for FF innerText for the rest 
-                cell = $.trim(cell);
-                if(cell == AmpelAText) {
-                    ampel = i;
-                }
-                if(cell == AmpelTText) {
-                    termin = i;
-                }
-                if(reg != false && cell == AmpelDText) {
-                    done = i;
-                }
-            }
-            // Ohne "Ampel" und "Termin" laeuft das Plugin nicht, "Done" ist optional
-            if(termin == -1 || ampel == -1) {
-                log("Light with id '" + AmpelID + "': Column for date, or destination not found!");
+        // Wenn das Objekt mit AmpelID ein div ist, benutze (erste) Tabelle darin
+        if(tabellen.tagName.toUpperCase() == "DIV") {
+            tabellen = tabellen.getElementsByTagName("table");
+            if(tabellen.length != 0) {
+                tabellen = tabellen[0];
+            } else {
+                log("No table found in div with id '" + AmpelID + "'!");
                 continue;
             }
+        }
 
-            var d = datum; // Wird das Ablaufdatum halten
+        var zeilen = tabellen.rows; 
 
-            // Gehe alle Zeilen durch und setze ggf. Ampel
-            try {
-                for (var zeileNr = 1; zeileNr < zeilen.length; zeileNr++) {
-                    var zeile = zeilen[zeileNr];
-                    var str = "";
-                    if(zeile.cells === undefined) {
-                        // Kein Fehler, einfach eine leere Zelle
+        var head = zeilen[0]; // Kopfzeile
+
+        var termin = -1; // Spaltennummer Termin
+        var ampel = -1; // Spaltennummer Ampel
+        var done = -1; // Spaltennummer Erledigt
+        var reg = false; // RegEx zum pruefen von Erledigt
+
+        // Pruefe ob optionale Erledigt-Spalte vorhanden
+        if(AmpelDText != "" && AmpelDCond != "") {
+            reg = new RegExp(AmpelDCond,"i");
+        }    
+
+        // Suche Spaltennummern raus
+        for (var i = 0; i < head.cells.length; i++) {
+            var cell = head.cells[i].textContent || head.cells[i].innerText; // textContent for FF innerText for the rest 
+            cell = $.trim(cell);
+            if(cell == AmpelAText) {
+                ampel = i;
+            }
+            if(cell == AmpelTText) {
+                termin = i;
+            }
+            if(reg != false && cell == AmpelDText) {
+                done = i;
+            }
+        }
+        // Ohne "Ampel" und "Termin" laeuft das Plugin nicht, "Done" ist optional
+        if(termin == -1 || ampel == -1) {
+            log("Light with id '" + AmpelID + "': Column for date, or destination not found!");
+            continue;
+        }
+
+        // Gehe alle Zeilen durch und setze ggf. Ampel
+        try {
+            for (var zeileNr = 1; zeileNr < zeilen.length; zeileNr++) {
+                var d = null; // Wird das Ablaufdatum halten
+                var zeile = zeilen[zeileNr];
+                var str = "";
+                if(zeile.cells === undefined) {
+                    // Kein Fehler, einfach eine leere Zelle
+                    continue;
+                }
+
+                // Pruefe, ob Aufgabe abgeschlossen
+                if(done > 0) {
+                    str = $.trim(zeile.cells[done].textContent || zeile.cells[done].innerText);
+                    if(reg.test(str)) {
+                        zeile.cells[ampel].innerHTML = getTag(0, str); 
                         continue;
-                    }
-
-                    // Pruefe, ob Aufgabe abgeschlossen
-                    if(done > 0) {
-                        str = $.trim(zeile.cells[done].textContent || zeile.cells[done].innerText);
-                        if(reg.test(str)) {
-                            zeile.cells[ampel].innerHTML = getTag(0, str); 
-                            continue;
-                        }
-                    }
-
-                    // Suche Termin-Datum
-                    str = $.trim(zeile.cells[termin].innerHTML);
-                    try { 
-                        // Datum rausssuchen
-                        var split = /(\d\d?)\.(\d\d?)\.(\d{2,4})?/.exec(str);
-                        if(split === null) {
-                            // Check for warn-words
-                            if(WCond != null && WCond.test(str)) {
-                                d = datum;
-                            } else {
-                                // Englisches Format?
-                                split = /(\d\d?) (\w{3}) (\d{2,4})/.exec(str);
-                                if(split !== null) {
-                                    var m = split[2];
-                                    var monthArray = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", "err");
-                                    var gefunden = 0;
-                                    for(var i = 0; i < monthArray.length; i++) {
-                                        if(m == monthArray[i]) {
-                                            m = i;
-                                            gefunden = 1;
-                                            break;
-                                        }
-                                    }
-                                    if(gefunden == 0) continue;
-                                    d = new Date(split[3], m, split[1]);
-                                } else {
-                                    continue;
-                                }
-                            }
-                        } else {    
-                            if(split[3] === undefined || split[3] === "") { // Falls ohne Jahr: 27.07.
-                                split[3] = datum.getFullYear(); // Kann auch 2-Stellig sein (IE)
-                            }
-                            if(split[3] < 2000) split[3] = Number(split[3]) + 2000; // JJ in JJJJ umwandeln
-
-                            d = new Date(split[3], split[2]-1, split[1], 23, 59);
-                        }
-                    }
-                    catch(e)
-                    {
-                        log("Error while parsing date '" + str + "': " + e);
-                        continue;
-                    }
-
-                    var tage = (d.getTime() - datum.getTime())/86400000;
-                    if (tage < 0) {
-                        zeile.cells[ampel].innerHTML = getTag(3, "schon "+Math.floor(-tage)+" Tage abgelaufen");
-                    } else if (tage <= AmpelWarn) {
-                        zeile.cells[ampel].innerHTML = getTag(2, "noch "+Math.floor(tage)+" Tage");
-                    } else {
-                        zeile.cells[ampel].innerHTML = getTag(1, "noch "+Math.floor(tage)+" Tage");
                     }
                 }
-            } catch (e) {
-                // Sometimes cells[...] can be undefined (ie. empty foswiki-tables have a hidden <tr> with only 1 <td>, or with colspans)
+
+                // Suche Termin-Datum
+                str = $.trim(zeile.cells[termin].innerHTML);
+
+                d = parseDate(str);
+
+                // Check for warn-words
+                if(WCond != null && WCond.test(str)) {
+                    d = datum;
+                }
+                if(d === null) {
+                    // kein Datum gefunden
+                    continue;
+                }
+
+                var tage = (d.getTime() - datum.getTime())/86400000;
+                if (tage < 0) {
+                    zeile.cells[ampel].innerHTML = getTag(3, "schon "+Math.floor(-tage)+" Tage abgelaufen");
+                } else if (tage <= AmpelWarn) {
+                    zeile.cells[ampel].innerHTML = getTag(2, "noch "+Math.floor(tage)+" Tage");
+                } else {
+                    zeile.cells[ampel].innerHTML = getTag(1, "noch "+Math.floor(tage)+" Tage");
+                }
             }
-        } else {
-            log("Id '" + AmpelID + "' not found!");
+        } catch (e) {
+            // Sometimes cells[...] can be undefined (ie. empty foswiki-tables have a hidden <tr> with only 1 <td>, or with colspans)
         }
     }
 }
